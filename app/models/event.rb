737df645713309c52bee3af4276fc9e8d5027303
Brandon Yee <brandon.yee88@gmail.com>
2014-11-28 20:46:09 -0500

    fixing up relationships in the model:

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  belongs_to :group_id
-  belongs_to :who_created
-  belongs_to :who_updated
+  belongs_to :group
+  belongs_to :user, foreign_key: "who_created"
+  belongs_to :user, foreign_key: "who_updated"
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ActiveRecord::Base
-  belongs_to :who_created
-  belongs_to :who_updated
+  belongs_to :user, foreign_key: "who_created"
+  belongs_to :user, foreign_key: "who_updated"
   belongs_to :based_in
 end
