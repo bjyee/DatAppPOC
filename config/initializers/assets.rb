8c681ed5f575d00fede87f2981e2f08c0599afe9
Brandon Yee <brandon.yee88@gmail.com>
2014-11-28 23:54:09 -0500

    finally got js and css files to load

---
+++ config/initializers/assets.rb
@@ -0,0 +1,7 @@
+# Javascript
+Rails.application.config.assets.precompile += %w( jquery-1.11.1.min.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+
+
+# Stylesheets
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
